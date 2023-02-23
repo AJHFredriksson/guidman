a9a3f1dcd07ea04f5953f0b411e9abc67069c143
ajhfredriksson <andreasjanhannufredriksson@gmail.com>
2023-02-23 10:26:56 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        var guid = Guid.NewGuid().ToString().ToUpper();
+        Console.WriteLine(guid);
+        Console.WriteLine("Press 'C' key to copy guid to clipboard. Press any other key to cancel.");
+        if (Console.ReadKey(true).Key == ConsoleKey.C)
+        {
+            Clipboard.SetText(guid);
+            Console.WriteLine($"{guid} copied to clipboard!");
+        }
+    }
+}
